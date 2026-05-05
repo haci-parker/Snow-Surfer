9ad51fc86989e7525833b87268edc2ea64a92343
Mustafa Berkay Er <m.berkay.er@gmail.com>
2026-05-05 22:30:53 +0300

    Decorations, movement boost system, and particle system added

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -4,12 +4,15 @@ using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float restartDelay = 2f;
+    [SerializeField] ParticleSystem crashParticles;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         int layerIndex = LayerMask.NameToLayer("Floor");
         if(collision.gameObject.layer == layerIndex)
         {
             Debug.Log("Player has lost!");
+            crashParticles.Play();
             Invoke("ReloadScene",restartDelay);
         }
     }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,24 +4,52 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float baseSpeed = 15f;
+    [SerializeField] float boostSpeed = 30f;
+
     InputAction moveAction;
     Rigidbody2D myRigidbody2D;
+    SurfaceEffector2D surfaceEffector2D;
+
+    Vector2 moveVector;
 
     void Start()
-    {
-        myRigidbody2D = GetComponent<Rigidbody2D>();
+    {   
         moveAction = InputSystem.actions.FindAction("Move");
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindFirstObjectByType<SurfaceEffector2D>();
     }
 
     void Update()
     {
-        Vector2 moveVector;
+        RotatePlayer();
+        BoostPlayer();
+    }
+
+    void RotatePlayer()
+    {
         moveVector = moveAction.ReadValue<Vector2>();
         if(moveVector.x < 0){
             myRigidbody2D.AddTorque(torqueAmount);
         }
         if(moveVector.x > 0){
             myRigidbody2D.AddTorque(-torqueAmount);
+        }
+    }
+
+    void BoostPlayer()
+    {
+        if(moveVector.y > 0)
+        {
+            Debug.Log("y > 0");
+            surfaceEffector2D.speed = boostSpeed;
+            Debug.Log(surfaceEffector2D.speed);
+        }
+        else if(moveVector.y == 0)
+        {
+            Debug.Log("y = 0");
+            surfaceEffector2D.speed = baseSpeed;
+            Debug.Log(surfaceEffector2D.speed);
         }
     }
 }
