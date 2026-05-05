18b5fc370f783f5c58e8272bcfa6279759d0f235
Mustafa Berkay Er <m.berkay.er@gmail.com>
2026-05-05 11:14:30 +0300

    Add particle system

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,13 +1,19 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float restartDelay = 2f;
     void OnTriggerEnter2D(Collider2D collision)
     {
         int layerIndex = LayerMask.NameToLayer("Floor");
         if(collision.gameObject.layer == layerIndex)
         {
             Debug.Log("Player has lost!");
+            Invoke("ReloadScene",restartDelay);
         }
+    }
+    void ReloadScene(){
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,13 +1,19 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float restartDelay = 1f;
     void OnTriggerEnter2D(Collider2D collision)
     {
         int layerIndex = LayerMask.NameToLayer("Player");
         if(collision.gameObject.layer == layerIndex)
         {
             Debug.Log("The player has won!");
+            Invoke("ReloadScene",restartDelay);
         }
+    }
+    void ReloadScene(){
+        SceneManager.LoadScene(0);
     }
 }
