e587ef11ed7e79fcc6c262a650c285fdccf12adf
Mustafa Berkay Er <m.berkay.er@gmail.com>
2026-05-11 21:48:57 +0300

    Collectable boost item added

---
+++ Assets/Scripts/PowerupManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PowerupManager : MonoBehaviour
+{
+    [SerializeField] PowerupSO powerup;
+
+    PlayerController player;
+
+    void Start()
+    {
+        player = FindFirstObjectByType<PlayerController>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        int layerIndex = LayerMask.NameToLayer("Player");
+
+        if(collision.gameObject.layer == layerIndex)
+        {
+            // Activate the powerup
+            player.ActivatePowerup(powerup);
+        }
+    }
+}
